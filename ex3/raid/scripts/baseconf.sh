1829449e153ba1d5d495f2aa8a6c1b2ebbaf76f2
stadmin <root@otus23.workgroup>
2023-11-23 16:27:12 +0300

    ex3 v.1

---
+++ ex3/raid/scripts/baseconf.sh
@@ -0,0 +1,21 @@
+     sudo mkdir -p ~root/.ssh;
+     sudo cp ~vagrant/.ssh/auth* ~root/.ssh
+     sudo echo "PasswordAuthentication yes" > /etc/ssh/sshd_config
+     sudo echo "ChallengeResponseAuthentication no" >> /etc/ssh/sshd_config
+     sudo echo "X11Forwarding no" >> /etc/ssh/sshd_config
+     sudo echo "UsePAM yes" >> /etc/ssh/sshd_config
+     sudo echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+     sudo echo "PubkeyAuthentication yes" >> /etc/ssh/sshd_config
+     sudo echo "AuthorizedKeysFile      .ssh/authorized_keys" >> /etc/ssh/sshd_config
+     sudo echo "PermitEmptyPasswords no ">> /etc/ssh/sshd_config
+     sudo echo "Subsystem	sftp	/usr/libexec/openssh/sftp-server ">> /etc/ssh/sshd_config
+     sudo setenforce 0
+     sudo systemctl stop firewalld.service
+     sudo systemctl disable firewalld.service
+     sudo dnf install python3 -y
+     sudo dnf install mdadm smartmontools hdparm gdisk -y
+     sudo dnf install mc nano -y
+     sudo dnf install -y mdadm smartmontools hdparm gdisk
+     sudo usermod -aG wheel vagrant
+     sudo timedatectl set-timezone Europe/Moscow
+     sudo systemctl restart sshd   
---
+++ ex3/raid/scripts/raidinit.sh
@@ -0,0 +1,18 @@
+sudo mdadm --zero-superblock --force /dev/sd{b,c,d,e,f}
+yes | sudo mdadm --create --verbose /dev/md127 -l 1 -n 5 /dev/sd{b,c,d,e,f}
+sudo mkdir /etc/mdadm
+sudo mdadm --detail --scan --verbose | awk '/ARRAY/ {print}' >> /etc/mdadm/mdadm.conf
+
+sudo parted -s /dev/md127 mklabel gpt
+sudo parted /dev/md127 mkpart primary ext4 0% 20%
+sudo parted /dev/md127 mkpart primary ext4 20% 40%
+sudo parted /dev/md127 mkpart primary ext4 40% 60%
+sudo parted /dev/md127 mkpart primary ext4 60% 80%
+sudo parted /dev/md127 mkpart primary ext4 80% 100%
+
+for i in $(seq 1 5); do sudo mkfs.ext4 /dev/md127p$i; done
+
+mkdir -p /raid/part{1,2,3,4,5}
+
+for i in $(seq 1 5); do sudo mount /dev/md127p$i /raid/part$i; done
+for i in $(seq 1 5); do sudo echo "/dev/md127p$i      /raid/part$i     ext4    defaults    1 2" >> /etc/fstab; done
